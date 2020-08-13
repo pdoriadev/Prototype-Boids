e1b58f155564589eb9cc326e39b6518f53a48ff2
Pita Bread <pdoriadev@gmail.com>
2020-08-13 13:49:12 -0700

    Initial Project Commit

---
+++ Prototype-Boids/Assets/BallBoy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Runs around. Controlled by player
+public class BallBoy : MonoBehaviour
+{
+    public List<Ball> ballFollowers = new List<Ball>();
+    public float moveForce;
+
+    Rigidbody2D rb;
+    Vector2 moveDir;
+    float xDir;
+    float yDir;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Gets movement vector
+        xDir = Input.GetAxisRaw("Horizontal");
+        yDir = Input.GetAxisRaw("Vertical");
+        moveDir = new Vector2(xDir, yDir);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(moveDir * moveForce);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+        if (ball && !ballFollowers.Contains(ball))
+        {
+            ballFollowers.Add(ball);
+            ball.AssignMyBallBoy(this);
+            print("new ballboy");
+        }
+        
+    }
+
+    public int GetBallIndex(Ball ball)
+    {
+        int index = 0;
+        if (ballFollowers.Contains(ball))
+        {
+            index = ballFollowers.IndexOf(ball);
+        }
+        else Debug.LogError("Lil ball isn't in this ballboy's list. Will return false index.");
+
+        return index;
+    }
+
+    public Ball GetBall(int index)
+    {
+        Ball b = ballFollowers[index];
+        if (!b)
+            Debug.LogError("Error! No ball here!");
+
+        return b;
+    }
+}
---
+++ Prototype-Boids/Assets/BallEater.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallEater : MonoBehaviour
+{
+    public GameObject ballBoy;
+    public float moveSpeed;
+
+    Rigidbody2D Rb;
+    Transform BBTForm;
+    Vector2 MoveDir;
+
+    private void Awake()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+        BBTForm = ballBoy.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        MoveDir = new Vector2(BBTForm.position.x - transform.position.x, BBTForm.position.y - transform.position.y );
+        Rb.AddForce(MoveDir * moveSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+      
+
+    }
+}
